6017ac2ba56c3421b97ae9a7aee6ad0a702c7b94
wcscs_global <320971424@qq.com>
2019-11-20 11:27:57 +0800

    汪长爽第一次添加数据

---
+++ src/main/java/com/qfjy/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.qfjy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info (){
+        return "hello spring boot5";
+    }
+
+}
